f40176d0be772cf146a608549c7529de6a224cd7
René Schultz Madsen <rm@microting.com>
2019-03-20 10:51:11 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,3 +16,6 @@ echo $GITVERSION
 echo "################## END GITVERSION ##################"
 su ubuntu -c \
 "dotnet publish ServiceTrashInspectionPlugin.sln -o out /p:Version=$GITVERSION --runtime linux-x64 --configuration Release"
+
+su ubuntu -c \
+"cp -av /var/www/microting/eform-service-trashinspection-plugin/ServiceTrashInspectionPlugin/out /var/www/microting/eform-debian-service/MicrotingService/MicrotingService/out/Plugins/ServiceTrashInspectionPlugin"
