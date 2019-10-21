620cf4c91d406ae7b6361261f67b9ed7864701b7
René Schultz Madsen <rm@microting.dk>
2019-10-21 13:44:05 +0200

    Fixing the install.sh so the file will use the rabbitmqadmin in / instead of the current folder.

--- install.sh
+++ install.sh
@@ -22,4 +22,4 @@ su ubuntu -c \
 
 su ubuntu -c \
 "cp -av /var/www/microting/eform-service-trashinspection-plugin/ServiceTrashInspectionPlugin/out /var/www/microting/eform-debian-service/MicrotingService/MicrotingService/out/Plugins/ServiceTrashInspectionPlugin"
-./rabbitmqadmin declare queue name=eform-service-trashinspection-plugin durable=true
+/rabbitmqadmin declare queue name=eform-service-trashinspection-plugin durable=true
